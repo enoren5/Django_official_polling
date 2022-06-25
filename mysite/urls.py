f243acf1653afaecf9dde8cc71208f0335df35d8
Pete Naveen Pearson <warden496@protonmail.com>
2022-06-25 06:09:00 -0400

    Initialize class based view + about.html

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,3 +1,14 @@
+from django.contrib import admin
+from django.urls import path, include
+from polls import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('polls/', include('polls.urls')),
+    path('about/', views.AboutView.as_view()),
+]
+
+
 """mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -13,10 +24,3 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
-]
