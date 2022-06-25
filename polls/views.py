fec50ef801fc7b1f6cecad721a4db1d9bfe5cf22
Pete Naveen Pearson <warden496@protonmail.com>
2022-06-25 03:24:31 -0400

    Initialize index.html template and build views.py

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Question
+# from django.template import loader
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+    
+    '''latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+    '''
+
+    '''.
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
     output = '<br> '.join([q.question_text for q in latest_question_list])
     return HttpResponse(output)
-
+    '''
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+    # return HttpResponse(f"You're looking at question {question_id}")
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
