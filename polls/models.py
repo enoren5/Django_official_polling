e2a3653135c8b933f251566b8665d9eea59d227f
Pete Naveen Pearson <warden496@protonmail.com>
2022-07-15 08:07:01 -0400

    Re-implement 'related_name' ForeignKey paramater

--- polls/models.py
+++ polls/models.py
@@ -14,7 +14,11 @@ class Question(models.Model):
         return self.question_text
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE,)
+    question = models.ForeignKey(
+        Question, 
+        on_delete=models.CASCADE,
+        related_name='entries',
+        )
     choice_text = models.CharField(max_length=200)
     notes = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
